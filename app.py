e4b97aa3efc7c428363c723dfda12db989b57bf0
ShUbHaMHiReMaT <shubhamhiremath12@gmail.com>
2025-08-03 01:16:44 +0530

    Update app.py
    made a bit changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-# Route to serve the Gemba Walk UI
+
 @app.route('/')
 def gogemba():
     return render_template('GoGemba.html')
